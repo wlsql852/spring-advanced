bf1fc6675e5cc466bd2bf21d39bed986e459d325
wlsql852 <wlsql852@naver.com>
2024-09-13 11:15:04 +0900

    1-9 수정

--- src/main/java/org/example/expert/config/AdminLogAop.java
+++ src/main/java/org/example/expert/config/AdminLogAop.java
@@ -1,22 +1,27 @@
 package org.example.expert.config;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.example.expert.domain.common.dto.AuthUser;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 @Aspect
 @Slf4j
 public class AdminLogAop {
+
+    private final HttpServletRequest httpRequest;
+    private final JwtUtil jwtUtil;
+
+    public AdminLogAop(HttpServletRequest httpRequest, JwtUtil jwtUtil) {
+        this.httpRequest = httpRequest;
+        this.jwtUtil = jwtUtil;
+    }
+
 
     @Pointcut("execution(* org.example.expert.domain.comment.controller.CommentAdminController.deleteComment(..))")
     private void commentDelete() {}
@@ -26,18 +31,17 @@ public class AdminLogAop {
 
     @Around("commentDelete()||changeUserRole()")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         try {
             //핵심기능 수행
             return joinPoint.proceed();
         }finally {
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
             // 로그인 회원 정보
-            AuthUser userDetails = (AuthUser) auth.getPrincipal();
+            String bearerJwt = httpRequest.getHeader("Authorization");
+            String jwt = jwtUtil.substringToken(bearerJwt);
+            Claims claims = jwtUtil.extractClaims(jwt);
+            Long userId = Long.parseLong(claims.getSubject());
 
-            log.info("user's id : "+userDetails.getId()+", api request time : " + System.currentTimeMillis()+", api request url : "+request.getRequestURI());
+            log.info("user's id : "+userId+", api request time : " + System.currentTimeMillis()+", api request url : "+httpRequest.getRequestURI());
         }
-
     }
 }
